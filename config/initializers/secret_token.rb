c17c1fa7e533bcc8cfc113f461ecd963c1a696b1
Michael Gillis <michael@uken.com>
2012-04-22 18:10:40 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lightwire::Application.config.secret_token = '44666ce7c64e8fb4bfe03344cc739aa36f2bfb696f4f7719ebc77a4498b41fb5385889a276cfc164247e7b7a79fbe94435267575aa0993c9b7b4b480bbddcb36'
